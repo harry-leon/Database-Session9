44470bb8a9df3c63d8f091b9fd0b57f6725be7d6
Ha Thuc Quoc Hung <htquochung.dev@gmail.com>
2026-04-27 23:24:54 +0700

    Homework 1

--- Homeword1/Homeword1.sql
+++ Homeword1/Homeword1.sql
@@ -1,1 +1,20 @@
-chỉnh sửa
+create table if not exists orders (
+    order_id serial primary key,
+    customer_id int not null,
+    order_date date,
+    total_amount numeric
+);
+
+drop index if exists idx_orders_customer_id;
+
+explain analyze
+select *
+from orders
+where customer_id = 1;
+
+create index idx_orders_customer_id on orders using btree (customer_id);
+
+explain analyze
+select *
+from orders
+where customer_id = 1;
